a5bb0149e29574a888809924e839b5e3af5488ff
Juan Castro <info@jempe.org>
2021-10-13 20:47:51 -0500

    detect faces

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
 	"log"
 	"net/http"
 	"os"
@@ -50,6 +52,20 @@ func mjpegCapture() {
 	img := gocv.NewMat()
 	defer img.Close()
 
+	// color for the rect when faces detected
+	blue := color.RGBA{0, 0, 255, 0}
+
+	// load classifier to recognize faces
+	classifier := gocv.NewCascadeClassifier()
+	defer classifier.Close()
+
+	xmlFile := "classifiers/haarcascade_frontalface_default.xml"
+
+	if !classifier.Load(xmlFile) {
+		fmt.Printf("Error reading cascade file: %v\n", xmlFile)
+		return
+	}
+
 	for {
 		if ok := webcam.Read(&img); !ok {
 			fmt.Printf("Device closed:")
@@ -57,6 +73,20 @@ func mjpegCapture() {
 		}
 		if img.Empty() {
 			continue
+		}
+
+		// detect faces
+		rects := classifier.DetectMultiScale(img)
+		fmt.Printf("found %d faces\n", len(rects))
+
+		// draw a rectangle around each face on the original image,
+		// along with text identifying as "Human"
+		for _, r := range rects {
+			gocv.Rectangle(&img, r, blue, 3)
+
+			size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
+			pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
+			gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
 		}
 
 		buf, _ := gocv.IMEncode(".jpg", img)
